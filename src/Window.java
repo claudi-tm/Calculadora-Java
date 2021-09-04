6ea77f74fff72915bb5085fbe598e3a3782b4855
claudi-tm <ccaducarlos@gmail.com>
2021-09-04 22:48:40 +0200

    second commit

---
+++ src/Window.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Window {
+    private JFrame frame;
+
+    public Window(){
+        frame = new JFrame();
+        frame.setTitle("Calculadora");
+        //frame.setSize(d);
+    }
+}
